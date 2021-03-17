8e97a9bda3d623ae8cc012a139467f4a334d1ade
ji sun lee <ellijs@hotmail.com>
2021-03-17 22:29:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 def position_taken?(board, index)
   if
-    array[index] == " "|| array[index] == "" || array[index] == nil
+    board[index] == " "|| board[index] == "" || board[index] == nil
     return false
   else
     return true
